fab5487c27c539898681492f6b31aa62da9397cd
dlange <dawid.lange@lppsa.com>
2016-10-04 16:48:22 +0200

    Cz4 dodanie nowych obiektow activity

--- crm_bootstrap.php
+++ crm_bootstrap.php
@@ -7,8 +7,21 @@
  */
 
 include_once 'model/Activity.php';
+include_once 'model/Company.php';
+include_once 'model/Salesman.php';
 
-$activity = new Activity(1,'jajka','2016-09-26','Pietrek','Kurak-Z.O.O',ActivityType::PHONE,ActivityStatus::IN_PROGRESS,null);
+$salesman = new Salesman(1,"Brunon Nosek");
+$salesmanTwo = new Salesman(2,"Zenek Kowalski");
+
+
+$company = new Company(1, "Borsukowo Z.O.O");
+$companyTwo = new Company(2, "Kurkowo Z.O.O");
+$companyThree = new Company(3, "Zukowo Z.O.O");
+
+$activity = array(
+    new Activity(1,'jajka','2016-09-26',$salesman->getInfo(),$company->getInfo(),ActivityType::PHONE,ActivityStatus::IN_PROGRESS,null),
+    new Activity(2,'koszyki','2016-10-22',$salesmanTwo->getInfo(),$companyTwo->getInfo(),ActivityType::EMAIL,ActivityStatus::CLOSED,'fajny misio'),
+);
 
 ?>
 
