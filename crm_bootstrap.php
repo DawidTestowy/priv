cba9127734f99fc79eb2089afd7b29dcc9dabf2e
dlange <dawid.lange@lppsa.com>
2016-10-10 17:58:51 +0200

    dodanie nowych obiektow

--- crm_bootstrap.php
+++ crm_bootstrap.php
@@ -12,6 +12,7 @@ include_once 'model/Salesman.php';
 
 $salesman = new Salesman(1,"Brunon Nosek");
 $salesmanTwo = new Salesman(2,"Zenek Kowalski");
+$salesmanThre = new Salesman(3,"Zdzisław Pakman");
 
 
 $company = new Company(1, "Borsukowo Z.O.O");
@@ -20,7 +21,9 @@ $companyThree = new Company(3, "Zukowo Z.O.O");
 
 $activitys = array(
     new Activity(1,'jajka','2016-09-26',$salesman->getInfo(),$company->getInfo(),ActivityType::PHONE,ActivityStatus::IN_PROGRESS,null),
-    new Activity(2,'koszyki','2016-10-22',$salesmanTwo->getInfo(),$companyTwo->getInfo(),ActivityType::EMAIL,ActivityStatus::CLOSED,'fajny misio')
+    new Activity(2,'koszyki','2016-10-22',$salesmanTwo->getInfo(),$companyTwo->getInfo(),ActivityType::EMAIL,ActivityStatus::CLOSED,'fajny misio'),
+    new Activity(3,'owce','2016-10-09',$salesmanThre->getInfo(),$companyThree->getInfo(),ActivityType::EMAIL,ActivityStatus::CLOSED,null),
+    new Activity(4,'makaron','2016-10-12',$salesmanTwo->getInfo(),$companyTwo->getInfo(),ActivityType::MEETING,ActivityStatus::OPEN,'fajny misio')
 );
 
 ?>
